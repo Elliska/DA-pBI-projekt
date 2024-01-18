f059b72e637c3b822d12455e8f618267b9a7c2a4
Ella <michaela.malec@gmail.com>
2024-01-18 15:44:36 +0100

    doplněno do plně funkčního stavu

--- date_table.py
+++ date_table.py
@@ -1,21 +1,47 @@
+import pyodbc
+from sqlalchemy import create_engine
 import pandas as pd
 from datetime import datetime, timedelta
 import holidays
-import os
 
 
-script_dir = os.path.dirname(os.path.realpath(__file__))
+dim_date = 'dimDate'
 
-# Nastavení názvu souboru
-file_name = "date_export.csv"
+################ DB connection
+server = 'localhost'
+database = 'weather'
+trusted_connection = 'yes'
+conn = pyodbc.connect(f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};TRUSTED_CONNECTION={trusted_connection}')
+cursor = conn.cursor()
 
-# Vytvoření cesty k souboru ve stejném adresáři jako skript
-file_path = os.path.join(script_dir, file_name)
+print('Spojení s DB navázáno.')
 
+create_table_script = f'''
+    IF OBJECT_ID('{dim_date}', 'U') IS NULL
+    BEGIN
+        CREATE TABLE {dim_date} (
+        ID_date INT PRIMARY KEY,
+        date_name DATE,
+        date_of_week NVARCHAR(20),
+        quarter INT,
+        holiday NVARCHAR(1),
+        holiday_name NVARCHAR(50),
+        season NVARCHAR(20),
+        week_num INT,
+        year INT,
+        month INT,
+        day INT
+    );
+    END
+'''
+cursor.execute(create_table_script)
+conn.commit()
+
+print(f'Tabulka {dim_date} byla vytvořena.')
 
 # Generování kalendářních dat
 start_date = datetime(2000, 1, 1)
-end_date = datetime(2025, 12, 31)
+end_date = datetime(2035, 12, 31)
 current_date = start_date
 
 cz_holidays = holidays.Czechia()
@@ -37,9 +63,9 @@ while current_date <= end_date:
         'season': 'Zima' if current_date.month in [12, 1, 2] else 'Jaro' if current_date.month in [3, 4, 5] else
                   'Léto' if current_date.month in [6, 7, 8] else 'Podzim' if current_date.month in [9, 10, 11] else None,
         'week_num': current_date.isocalendar()[1],
-        'year': int(current_date.strftime('%Y')),
-        'month': int(current_date.strftime('%m')),
-        'day': int(current_date.strftime('%d')),
+        'year':int(current_date.strftime('%Y')),
+        'month':int(current_date.strftime('%m')),
+        'day':int(current_date.strftime('%d')),
     })
 
     current_date += timedelta(days=1)
@@ -47,11 +73,24 @@ while current_date <= end_date:
 print('Data vygenerována. Vytvářím DataFrame...')
 
 # Vytvoření DataFrame
+engine = create_engine(f'mssql+pyodbc://{server}/{database}?driver=SQL+Server+Native+Client+11.0')
 df = pd.DataFrame(data)
 
-print('DataFrame vytvořen. Vkládám data do CSV souboru...')
+print('DataFrame vytvořen. Vkládám data do SQL Serveru...')
 
-# Export do CSV souboru
-df.to_csv(file_path, index=False)
+chunksize = 100
+for i in range(0, len(df), chunksize):
+    df_chunk = df.iloc[i:i + chunksize]
+    df_chunk.to_sql(dim_date, engine, index=False, if_exists='append')
 
-print(f'Data byla exportována do CSV souboru: {file_path}.')
+    progress_percentage = (i + len(df_chunk)) / len(df) * 100
+    print(f'Postup: {progress_percentage:.2f}%')
+
+# Vložení dat do SQL Serveru
+df.to_sql(dim_date, engine, index=False, if_exists='replace')
+
+print('Databyla vložena do DB')
+# Uzavření připojení
+conn.close()
+
+print(f'Skript pro {dim_date} byl dokončen.')
--- transformace_dat.py
+++ transformace_dat.py
@@ -3,7 +3,8 @@ from functools import reduce
 import pyodbc
 from unidecode import unidecode 
 #################
-in_path = 'C:/Users/michaela.maleckova/OneDrive - Seyfor/Projekt/DA-pBI-projekt/source_data/B2BTUR01_2.xlsx'
+# Relativní cesta jako obvykle odmítá fungovat
+in_path = 'C:/Users/michaela.maleckova/OneDrive - Seyfor/Projekt/DA-pBI-projekt/source_data/B2BTUR01.xlsx'
 out_path = 'C:/Users/michaela.maleckova/OneDrive - Seyfor/Projekt/DA-pBI-projekt/export.xlsx'
 out_path_info = 'C:/Users/michaela.maleckova/OneDrive - Seyfor/Projekt/DA-pBI-projekt/dimStanice.xlsx'
 
@@ -15,6 +16,10 @@ title = pd.read_excel(in_path, sheet_name=0, skiprows=2)
 info_table = title.dropna()
 info_table.columns = [unidecode(col) for col in info_table.columns]
 info_table.columns = [col.replace(' ', '_') for col in info_table.columns]
+
+#info_table.replace({'konec_pozorovani': 'dosud'}, {'konec_pozorovani':'2035-12-31'})
+info_table['konec_pozorovani'].replace({'dosud': '2035-12-31'}, inplace=True)
+
 
 print(info_table)
 
@@ -64,12 +69,90 @@ final = pd.merge(merged_df,df_result, on='datum').dropna()
 
 print(final)
 
-# Do excelu
-# final.to_excel(out_path, index=False)
-# info_table.to_excel(out_path_info)
 
-#########################
-# dynamické generování sloupců databáze, kde vím pouze datový typ?
-# variabliní množství sloupců v databázi podle množství tabulek
-# jak vyřešit název sloupců aby nebyl tak "krásný" (název listu?)
-# odseknout všechny databázové věci až na konec skriptu, až data zformátuje, ať se s tím pracuje snáz
+# Připojení k databázi
+server = 'localhost'
+database = 'weather'
+trusted_connection = 'yes' 
+conn = pyodbc.connect(f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};TRUSTED_CONNECTION={trusted_connection}')
+cursor = conn.cursor()
+
+print('Spojení s DB navázáno')
+
+
+table_data = 'factData'
+table_station = 'dimStation'
+
+
+should_update_table = True
+
+if should_update_table:
+    drop_table_script = f'DROP TABLE IF EXISTS {table_data};'
+    cursor.execute(drop_table_script)
+
+if should_update_table:
+    drop_table_script = f'DROP TABLE IF EXISTS {table_station};'
+    cursor.execute(drop_table_script)
+
+create_table_query = f'''
+IF OBJECT_ID('{table_data}', 'U') IS NULL
+BEGIN
+    CREATE TABLE {table_data} (
+        id_note INT PRIMARY KEY IDENTITY(1,1),
+        datum DATE,
+        teplota_prumerna FLOAT,
+        teplota_maximalni FLOAT,
+        teplota_minimalni FLOAT,
+        rychlost_vetru FLOAT,
+        tlak_vzduchu FLOAT,
+        vlhkost_vzduchu INT,
+        uhrn_srazek FLOAT,
+        celkova_vyska_snehu INT,
+        slunecni_svit FLOAT,
+        stanice NVARCHAR(50)
+    );
+        CREATE TABLE {table_station} (
+        id_note INT PRIMARY KEY IDENTITY(1,1),
+        indikativ_stanice NVARCHAR(100),
+        WMO_indikativ INT,
+        nazev_stanice NVARCHAR (100),
+        zacatek_pozorovani DATE,
+        konec_pozorovani DATE,
+        zemepisna_sirka VARCHAR (100),
+        zemepisna_delka VARCHAR (100),
+        nadmorska_vyska FLOAT,
+        povodi NVARCHAR (100),
+        typ_stanice NVARCHAR (100)
+
+    );
+END
+'''
+cursor.execute(create_table_query)
+print('Tabulky vytvořeny')
+
+columns_data = ('datum', 'teplota_prumerna', 'teplota_maximalni', 'teplota_minimalni', 'rychlost_vetru', 'tlak_vzduchu', 'vlhkost_vzduchu', 'uhrn_srazek', 'celkova_vyska_snehu', 'slunecni_svit', 'stanice')
+columns_info = ('indikativ_stanice', 'WMO_indikativ', 'nazev_stanice', 'zacatek_pozorovani', 'konec_pozorovani', 'zemepisna_sirka', 'zemepisna_delka', 'nadmorska_vyska', 'povodi', 'typ_stanice')
+
+# Spojení dohromady do jednoho cyklu klasika, nechce spolupracovat.
+for index, row in final.iterrows():
+    values_dict = row.to_dict()
+    insert_query = f"INSERT INTO {table_data} ({', '.join(columns_data)}) VALUES ({', '.join(['?']*len(columns_data))});"
+    values = [values_dict[column] for column in columns_data]
+    cursor.execute(insert_query, values)
+
+print(f'Data zapsaná do DB, tabulka {table_data}')
+
+for index, row in info_table.iterrows():
+    values_dict = row.to_dict()
+    insert_query = f"INSERT INTO {table_station} ({', '.join(columns_info)}) VALUES ({', '.join(['?']*len(columns_info))});"
+    values = [values_dict[column] for column in columns_info]
+    cursor.execute(insert_query, values)
+
+print(f'Data zapsaná do DB, tabulka {table_station}')
+# Potvrzení změn v databázi
+conn.commit()
+conn.close()
+
+print('Hotovo')
+
+
